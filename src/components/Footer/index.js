7b287e69f5559dbdbeea2fb1aa9a2249edcf6671
Mbabou Romario Ulrich <70371226+Rpmario@users.noreply.github.com>
2023-02-21 23:14:25 +0100

    commit 2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,75 @@
 import React from 'react';
+import Link from "next/link";
+import { GlobeAltIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
 import styles from "./index.module.scss";
 
 const Index = () => {
   return (
-    <footer className={styles.footer__main}>
-      <div className={styles.footer__text}>
-        &copy; 2023 Ynov Paris
+    <footer>
+      <span className={styles.footer__space}>
+      </span>
+      <div className={styles.footer__main}>
+        <div className={styles.footer__box}>
+          <div className={styles.footer__bloc1}>
+            <ol>
+            <li>
+                <span href="#" className={styles.footer__right}>
+                &copy; 2023  Airbnb, Inc.
+                </span>
+              </li>
+              <li>
+                <span>.</span>
+                <Link href="#" className={styles.footer__link}>
+                  Conditions générales
+                </Link>
+              </li>
+              <li>
+                <span>.</span>
+                <Link href="#" className={styles.footer__link}>
+                  Plan du site
+                </Link>
+              </li>
+              <li>
+                <span>.</span>
+                <Link href="#" className={styles.footer__link}>
+                  Fonctionnement du site
+                </Link>
+              </li>
+              <li>
+                <span>.</span>
+                <Link href="#" className={styles.footer__link}>
+                  Infos dur l'entreprise
+                </Link>
+              </li>
+              <li>
+                <span>.</span>
+                <Link href="#" className={styles.footer__link}>
+                  Destinations
+                </Link>
+              </li>
+            </ol>
+          </div>
+          <div className={styles.footer__bloc2}>
+            <Link href="#" className={styles.footer__l} >
+              <GlobeAltIcon color="black" height={17} className={styles.footer__icon} />
+            </Link>
+            <Link href="#" className={styles.footer__link2}>
+              Français (FR)
+            </Link>
+            <Link href="#" className={styles.footer__l} >
+              <span className={styles.footer__euro}>€</span>
+            </Link>
+            <Link href="#" className={styles.footer__link2}>
+              EUR
+            </Link>
+            <Link href="#" className={styles.footer__link2}>
+              Assistance et ressources
+            </Link>
+            <Link href="#" className={styles.footer__l}>
+              <ChevronUpIcon color="black" height={19} className={styles.footer__icon} />
+            </Link>
+          </div>
+        </div>
       </div>
     </footer>
   );
--- src/components/PlaceCard/index.js
+++ src/components/PlaceCard/index.js
@@ -1,9 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import styles from "./index.module.scss";
-//import HearthIcon from "../../../public/heart-light.svg";
 import WishlistContext from "../../context/WishlistContext";
 import { StarIcon, HeartIcon } from '@heroicons/react/24/solid';
-
 
 const Index = ({ place }) => {
 
@@ -27,7 +25,6 @@ const updateWish = () => {
             }
           }
         >
-          {/* <img src={HearthIcon.src} alt="favoris" /> */}
           {clicked? <HeartIcon onClick={() => updateWish()} className={styles.coeur} color="red"/>:
           <HeartIcon onClick={() => updateWish()} className={styles.coeur} color="gray"/>}
         </button>
@@ -35,8 +32,10 @@ const updateWish = () => {
       </div>
       <div className={styles.content}>
         <div className={styles.metadata}>
-          <p>{place.title}</p>
-          <p><b>{place.pricing.perDay}</b> € par nuit</p>
+          <p><b>{place.title}</b></p>
+          <p>{place?.proprio || "Particulier"}</p>
+          <p>{place?.periode || "3-9 avril"}</p>
+          <p><b>{place.pricing.perDay + " €"}</b>  par nuit</p>
         </div>
         <div className={styles.rate}>
           <StarIcon className={styles.star__rate} color="black"/>
